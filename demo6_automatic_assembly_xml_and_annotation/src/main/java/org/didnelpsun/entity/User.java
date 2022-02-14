4126a7659b436e174fd8adc51ca8dfccc7e411e8
Didnelpsun <2675350965@qq.com>
2022-02-14 23:43:18 +0800

    更新案例六

---
+++ demo6_automatic_assembly_xml_and_annotation/src/main/java/org/didnelpsun/entity/User.java
@@ -0,0 +1,43 @@
+// User.java
+package org.didnelpsun.entity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class User {
+    private String name;
+    private Integer age;
+    private HelloWorld helloWorld;
+    public User(){
+        this.name = "";
+        this.age = 0;
+        this.helloWorld = null;
+        System.out.println("UserClass");
+    }
+    public User(int age){
+        this();
+        this.age = age;
+    }
+    public User(HelloWorld helloWorld){
+        this();
+        this.helloWorld = helloWorld;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public HelloWorld getHelloWorld() {
+        return helloWorld;
+    }
+    @Autowired
+    public void setHelloWorld(HelloWorld helloWorld) {
+        this.helloWorld = helloWorld;
+    }
+}
